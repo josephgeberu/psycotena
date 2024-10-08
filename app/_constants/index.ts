a2132acffb85dd3b277c3e234cd488a7307c9d35
josephgeberu <josephgeberu@gmail.com>
2024-10-08 12:37:20 +0300

    create constants

---
+++ app/_constants/index.ts
@@ -0,0 +1,112 @@
+export const serviceContent = [
+  {
+    title: "Group Therapy",
+    body: "Offers a supportive environment for individuals with similar concerns to connect, share experiences, and receive support from one another.",
+    iconUrl: "/assets/icons/S4.svg", //When using Next.js, the public directory is served at the root
+  },
+  {
+    title: "Individual Therapy",
+    body: "This involves one-on-one sessions with clients to address their mental health concerns.",
+    iconUrl: "/assets/icons/S1.svg", //When using Next.js, the public directory is served at the root
+  },
+  {
+    title: "Family Therapy",
+    body: "Involves working with families to address issues that affect their dynamics, such as communication problems, parenting challenges, or substance abuse.",
+    iconUrl: "/assets/icons/S3.svg", //When using Next.js, the public directory is served at the root
+  },
+  {
+    title: "Couples Therapy",
+    body: "Focuses on helping couples improve their communication, resolve conflicts, and strengthen their relationship",
+    iconUrl: "/assets/icons/S2.svg", //When using Next.js, the public directory is served at the root
+  },
+];
+
+export const testimonials = [
+  {
+    rating: "⭐",
+    body: "TenaPsyco was a lifeline for me during a difficult time. Their compassionate and skilled therapists helped me navigate through my challenges and find healing.",
+
+    autor: "Joseph",
+  },
+  {
+    rating: "⭐",
+    body: "TenaPsyco was a lifeline for me during a difficult time. Their compassionate and skilled therapists helped me navigate through my challenges and find healing.",
+
+    autor: "Joseph",
+  },
+  {
+    rating: "⭐",
+    body: "TenaPsyco was a lifeline for me during a difficult time. Their compassionate and skilled therapists helped me navigate through my challenges and find healing.",
+
+    autor: "Joseph",
+  },
+  {
+    rating: "⭐",
+    body: "TenaPsyco was a lifeline for me during a difficult time. Their compassionate and skilled therapists helped me navigate through my challenges and find healing.",
+
+    autor: "Joseph",
+  },
+];
+
+export const psycatrists = [
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Joseph Gonzaleze",
+    titel: "Psycatrist",
+  },
+  {
+    photo: "/assets/icons/S3.svg",
+    name: "Hana Gonzaleze",
+    titel: "Psycatrist",
+  },
+];
